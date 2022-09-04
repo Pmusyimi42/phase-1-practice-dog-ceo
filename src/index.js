bebff085fa034913c41cbca506a3d489a0928bd1
patrick kisyula <pmusyimi42@gmail.com>
2022-09-04 23:12:50 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded',() => {
+    let allBreeds = [];
+ const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+ const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+ const dogImgContainer = document.getElementById("dog-image-container")
+ const dogBreedUl = document.getElementById("dog-breeds")
+ const breedDropdown = document.getElementById("breed-dropdown")
+ dogBreedUl.addEventListener("click", (event) => {
+    event.target.style.color = "blue"
+ });
+ breedDropdown.addEventListener("change", (event) => {
+    const letter = event.target.value
+    const filteredBreeds = allBreeds.filter(breed =>breed.startsWith(letter));
+    dogBreedUl.innerHTML = createDogList(filteredBreeds);
+ });
+ fetch(imgUrl)
+ .then(response => response.json())
+ .then((dogImgData) =>{
+    dogImgData.message.forEach(imgUrl => {
+        dogImgContainer.innerHTML += `<img src="${imgUrl}">`
+        
+    })
+    const dogImgString = dogImgData.message.map(imgUrl => `img src="${imgUrl}">`);
+ })
+
+   
+     
+     fetch(breedUrl)
+      .then(response => response.json())
+      .then(breedData => {
+        allBreeds = Object.keys(breedData.message)
+        console.log(allBreeds);
+        dogBreedUl.innerHTML = createDogList(allBreeds)
+      })
+    });
+      
+
